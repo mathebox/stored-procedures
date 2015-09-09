5c870c23b54df8d840667526849fc2396bec03fb
Max Bothe <mathebox@gmail.com>
2015-09-09 09:25:28 +0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='django-stored-procedures',
+    version='0.1',
+    description='Stored procedures for Django',
+    author='Jeroen Goudsmit',
+    author_email='jeroen@jeroengoudsmit.com',
+    url='https://github.com/jeroeng/stored-procedures',
+    packages=['django_stored_procedures'],
+)
